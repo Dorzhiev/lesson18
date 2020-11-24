090599b616df0f9e184d97e7d9ace6ed9f5b3587
Ayur Dorzhiev <ayurra@mail.ru>
2020-11-25 02:50:44 +0300

    fix

--- taimer2/script.js
+++ taimer2/script.js
@@ -1,4 +1,5 @@
-window.addEventListener('DOMContentLoaded', function(){
+const showTime = () => {
+
     'use strict';
 
 const welcome1 = document.querySelector('#say');
@@ -7,19 +8,19 @@ const myDate = new Date();
 const hour = myDate.getHours();
 let minute = myDate.getMinutes();
 let second = myDate.getSeconds();
-if (minute < 10) {
+    if (minute < 10) {
     minute = "0" + minute;
-}
-if (second < 10) {
+    }
+    if (second < 10) {
     second = "0" + second;
-}
-if (hour < 12) {
+    }
+    if (hour < 12) {
     welcome = "Доброе утро";
-} else if (hour < 17) {
+        } else if (hour < 17) {
     welcome = "Добрый день";
-} else {
+    } else {
     welcome = "вечер в хату";
-}
+    }
 welcome1.textContent = welcome;
 
 const hours = document.querySelector('#hours');
@@ -38,38 +39,23 @@ const dayOfWeeek = "Сегодня: " + day[realDate.getDay()];
 date.textContent = dayOfWeeek;
 
 function countTimer(deadline){
-    let timerHours = document.querySelector('#hours'),
-        timerMinutes = document.querySelector('#minutes'),
-        timerSeconds = document.querySelector('#seconds'),
-        timerDay = document.querySelector('#newYear');
+    let timerDay = document.querySelector('#newYear');
 
     function getTimeRemaining(){
     let dateStop = new Date(deadline).getTime(),
         dateNow = new Date().getTime(),
         timeRemaining = (dateStop - dateNow) / 1000,
-        seconds = Math.floor(timeRemaining % 60),
-        minutes = Math.floor((timeRemaining / 60) % 60),
-        hours = Math.floor(timeRemaining / 60 / 60),
         days = Math.floor(timeRemaining / 60 / 60/ 24); 
-        return {timeRemaining, hours, minutes, seconds, days};
+        return {timeRemaining, days};
     }
-            
-
     function updateClock(){
         let timer = getTimeRemaining();
-
-                // timerHours.textContent = ("0" + timer.hours).slice(-2);
-                // timerMinutes.textContent = ("0" + timer.minutes).slice(-2);
-                // timerSeconds.textContent = ("0" + timer.seconds).slice(-2);
         timerDay.textContent = "До нового года осталось " + ("0" + timer.days).slice(-2) + " дней";
                 
         if (timer.timeRemaining > 0) {
                 updateClockInterval;
             } else {
                 clearInterval(updateClockInterval);
-                timerHours.textContent="00";
-                timerMinutes.textContent="00";
-                timerSeconds.textContent="00";
                 timerDay.textContent="00";
             }  
                 
@@ -77,9 +63,10 @@ function countTimer(deadline){
     const updateClockInterval = setInterval(updateClock, 1000);
     updateClock();
     }
-
     countTimer('31 december 2020');
-});
+}
+setInterval(showTime, 1000);
+
 
 
 
