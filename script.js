768abf78c5a946e44e640fd97134fe4529c9ba63
Ayur Dorzhiev <ayurra@mail.ru>
2020-12-09 07:37:23 +0300

    lesson26

--- script.js
+++ script.js
@@ -357,21 +357,14 @@ const sendForm = () => {
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         form.appendChild(statusMessage);
+        statusMessage.textContent = loadMessage;
         const formData = new FormData(form);
         let body = {};
-
-        // for (let val of formData.entries()){
-        //     // console.log(val);
-        //     body[val[0]] = val[1]
-        // }
-
         formData.forEach((val, key) => {
             body[key] = val;
         });
-        postData(body, 
-            () => {
-                statusMessage.textContent = successMesage;
-
+        postData(body, () => {
+            statusMessage.textContent = successMesage;
         }, (error) => {
             statusMessage.textContent = errorMessage;
             console.error(error);
@@ -381,21 +374,17 @@ const sendForm = () => {
     const postData = (body, outputData, errorData) => {
         const request = new XMLHttpRequest();
         request.addEventListener('readystatechange', () => {
-            
             if (request.readyState !== 4) {
                 return;
             }
             if (request.status === 200) {
-                
+                outputData();
             } else {
-                erroreData(); 
+                erroreData(request.status); 
             }
         });
         request.open('POST', './server.php');
         request.setRequestHeader('Content-Type', 'application/json');
-        
-        console.log(body);
-
         request.send(JSON.stringify(body));
     }
 
