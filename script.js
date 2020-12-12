f0f8bb0cba73c57b0060be0b2b399f67f46b05ef
Ayur Dorzhiev <ayurra@mail.ru>
2020-12-12 22:31:19 +0300

    lesson29

--- script.js
+++ script.js
@@ -367,9 +367,14 @@ const sendForm = () => {
             body[key] = val;
         });
 
-        postData(body).then(() => {
-            statusMessage.textContent = successMesage;
-            setTimeout(() => {
+        postData(body)
+            .then((response) => {
+                if(response.status !== 200){
+                    throw new Error('status network not 200');
+                }
+                console.log(response)
+                statusMessage.textContent = successMesage;
+                setTimeout(() => {
                 statusMessage.textContent = '';
                 [...document.querySelectorAll('input')].forEach(item => item.value = '');
             }, 3000);
@@ -379,28 +384,20 @@ const sendForm = () => {
                 statusMessage.textContent = '';
                 [...document.querySelectorAll('input')].forEach(item => item.value = '');
             }, 3000);
-            console.error(error);
         });
     }));
 
     const postData = (body) => {
-
-         return new Promise((resolve, reject) => {
-            const request = new XMLHttpRequest();
-        request.addEventListener('readystatechange', () => {
-            if (request.readyState !== 4) {
-                return;
-            }
-            if (request.status === 200) {
-                resolve();
-            } else {
-                reject(request.status); 
-            }
-        });
-        request.open('POST', './server.php');
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify(body));
-    });
+        return fetch( './server.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body),
+            credentials: 'include',
+            cache: 'default'
+        })
+        
   };
     const formPhone = document.querySelectorAll('.form-phone');
     formPhone.forEach((item) => {
